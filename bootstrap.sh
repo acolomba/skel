da3c46de1d6aabed06522ec42e95b84379c71be5
Alessandro Colomba <acolomba@gmail.com>
2013-02-10 16:25:33 -0500

    installs base homebrew packages on os x if not already installed

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,34 @@ for df in "${dotfiles[@]}"; do
     fi
 
     cp "${df}" "${dst_df}"
-    # || { echo "Could not copy .${df} into home folder" }
+    # || { echo "Could not copy .${df} into home folder"; exit 1 }
 done
+
+for vd in "~/.vim/backups" "~/.vim/swaps"; do
+    if [[ ! -d $vd ]]; then
+        mkdir -p "$vd"
+    fi
+done
+
+if [[ $(uname) = 'Darwin' ]]; then
+    if which brew >/dev/null; then
+        # taps
+        brew tap homebrew/games >/dev/null
+
+        # if brew installed, checks that we have the base set of packages
+        for formula in elinks irssi mercurial nethack proxytunnel python python python tinyproxy unnethack unrar vim vim watch wget wget python; do
+            if [[ -z $(brew which $formula) ]]; then
+                # installs package if not already installed
+                brew install $formula
+            fi
+        done
+    else
+        echo >&2 "NOTE: brew is not installed"
+    fi
+fi
+
+
+# TODO
+# brew tap homebrew/games
+# brew install elinks install mercurial nethack proxytunnel python python python screen tinyproxy tsocks unnethack unrar vim vim watch wget wget python
 
